0b46faf14e078ae11110b509458b32bbcb1b39d2
nishaslidetech <nisha.dhiman@slidetech.in>
2022-08-24 12:22:00 +0530

    update captcha

--- src/test/java/SetupClass/TestStep/TakeImageStep.java
+++ src/test/java/SetupClass/TestStep/TakeImageStep.java
@@ -7,14 +7,13 @@ import SetupClass.Set;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 
-
-	public class TakeImageStep extends Set {
-		@After
-		public void tearDown(Scenario scenario) {
-		    if (scenario.isFailed()) {	    	
-		      log.info("Scenario failed, now taking screenshot");
-		      final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-		      scenario.embed(screenshot, "image/png");
-		    }
+public class TakeImageStep extends Set {
+	@After
+	public void tearDown(Scenario scenario) {
+		if (scenario.isFailed()) {
+			log.info("Scenario failed, now taking screenshot");
+			final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+			scenario.embed(screenshot, "image/png");
 		}
 	}
+}
--- src/test/java/SetupClass/TestStep/URL_Test.java
+++ src/test/java/SetupClass/TestStep/URL_Test.java
@@ -19,95 +19,93 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 
 public class URL_Test extends Set {
-	WebDriverWait wait = new WebDriverWait(driver,10);
-       JavascriptExecutor js = (JavascriptExecutor) driver;
-	
-	@Given("^user is already on sign up page cd$")
-	public void user_is_already_on_sign_up_page_cd() throws InterruptedException  {
-	    
-		/*driver.get("https://test2.slideteam.net/adminer.php");
-		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-		
-		Thread.sleep(3000);
-		
-		List<WebElement> dynamicElement = driver.findElements(By.xpath("//*[contains(text(),'Database')]"));
+	WebDriverWait wait = new WebDriverWait(driver, 10);
+	JavascriptExecutor js = (JavascriptExecutor) driver;
 
-		if (dynamicElement.size() != 0) {
-                               Assert.fail("Element is present");
-                                } else {
-                                  System.out.println("Element is not present");
-                                  }
-		Thread.sleep(3000);*/
-		
-	        driver.get("https://test1.slideteam.net/adminer.php");
+	@Given("^user is already on sign up page cd$")
+	public void user_is_already_on_sign_up_page_cd() throws InterruptedException {
+
+		/*
+		 * driver.get("https://test2.slideteam.net/adminer.php");
+		 * driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		 * 
+		 * Thread.sleep(3000);
+		 * 
+		 * List<WebElement> dynamicElement =
+		 * driver.findElements(By.xpath("//*[contains(text(),'Database')]"));
+		 * 
+		 * if (dynamicElement.size() != 0) { Assert.fail("Element is present"); } else {
+		 * System.out.println("Element is not present"); } Thread.sleep(3000);
+		 */
+
+		driver.get("https://test1.slideteam.net/adminer.php");
 		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-		
+
 		Thread.sleep(3000);
-		
+
 		List<WebElement> dynamicElement1 = driver.findElements(By.xpath("//*[contains(text(),'Database')]"));
 
 		if (dynamicElement1.size() != 0) {
-                               Assert.fail("Element is present");
-                                } else {
-                                  System.out.println("Element is not present");
-                                  }
-		
-	/*driver.get("https://test-admin.slideteam.net/adminer.php");
-		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-		
-		Thread.sleep(3000);
-		
-		List<WebElement> dynamicElement2 = driver.findElements(By.xpath("//*[contains(text(),'Database')]"));
+			Assert.fail("Element is present");
+		} else {
+			System.out.println("Element is not present");
+		}
 
-		if (dynamicElement2.size() != 0) {
-                               System.out.println("Element is present");
-                                } else {
-                                  System.out.println("Element is not present");
-                                  }*/
-		
+		/*
+		 * driver.get("https://test-admin.slideteam.net/adminer.php");
+		 * driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		 * 
+		 * Thread.sleep(3000);
+		 * 
+		 * List<WebElement> dynamicElement2 =
+		 * driver.findElements(By.xpath("//*[contains(text(),'Database')]"));
+		 * 
+		 * if (dynamicElement2.size() != 0) { System.out.println("Element is present");
+		 * } else { System.out.println("Element is not present"); }
+		 */
+
 		Thread.sleep(3000);
-		
-	       driver.get("https://www.slideteam.net/adminer.php");
+
+		driver.get("https://www.slideteam.net/adminer.php");
 		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-		
+
 		Thread.sleep(3000);
-		
+
 		List<WebElement> dynamicElement3 = driver.findElements(By.xpath("//*[contains(text(),'Database')]"));
 
 		if (dynamicElement3.size() != 0) {
-                              Assert.fail("Element is present");
-                                } else {
-                                  System.out.println("Element is not present");
-                                  }
-		
+			Assert.fail("Element is present");
+		} else {
+			System.out.println("Element is not present");
+		}
+
 		Thread.sleep(3000);
-	       driver.get("https://www4.slideteam.net/adminer.php");
+		driver.get("https://www4.slideteam.net/adminer.php");
 		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-		
+
 		Thread.sleep(3000);
-		
+
 		List<WebElement> dynamicElement4 = driver.findElements(By.xpath("//*[contains(text(),'Database')]"));
 
 		if (dynamicElement4.size() != 0) {
-                               Assert.fail("Element is present");
-                                } else {
-                                  System.out.println("Element is not present");
-                                  }
-		
+			Assert.fail("Element is present");
+		} else {
+			System.out.println("Element is not present");
+		}
+
 		Thread.sleep(3000);
-	        driver.get("https://www5.slideteam.net/adminer.php");
+		driver.get("https://www5.slideteam.net/adminer.php");
 		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-		
+
 		Thread.sleep(3000);
-		
+
 		List<WebElement> dynamicElement5 = driver.findElements(By.xpath("//*[contains(text(),'Database')]"));
 
 		if (dynamicElement5.size() != 0) {
-                              Assert.fail("Element is present");
-                                } else {
-                                  System.out.println("Element is not present");
-                                  }
-	
-		
+			Assert.fail("Element is present");
+		} else {
+			System.out.println("Element is not present");
+		}
+
 	}
 }
